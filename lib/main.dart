b525d71437ad028c1088cd14e12724b1b92169c9
Andrés Felipe Martínez Salazar <andresfelipe.2031@gmail.com>
2022-03-14 19:48:37 -0500

    Structural bugfix with BlocProvider and LoadingScreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,8 @@ import 'package:expense_record/Expenses/exprense.dart';
 import 'package:expense_record/Expenses/ui/screens/add_expense.dart';
 import 'package:expense_record/database.dart';
 import 'package:expense_record/widgets/loading_screen.dart';
-import 'package:floor/floor.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
-
-final waiterFuture = Future.delayed(const Duration(milliseconds: 2000), () {});
 
 void main() {
   runApp(const MyApp());
@@ -17,36 +14,38 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder<List<dynamic>>(
-        future: Future.wait([
-          /// Add here all initialization actions here before the main interface is displayed
-          waiterFuture,
-          AppDatabase.getInstanceFuture(),
-        ]),
-        builder: (context, snapshot) {
-          Widget child;
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            child = const LoadingScreen();
-          } else {
-            child = BlocProvider(
-              bloc: ExpensesBloc(),
-              child: const ExpensesPage(),
-            );
-          }
-          return AnimatedSwitcher(
-            duration: const Duration(seconds: 1),
-            child: child,
-          );
-        },
-      ),
-      routes: {
-        '/add_expense': (_) => const AddExpense(),
+    return FutureBuilder<List<dynamic>>(
+      future: preloadActions,
+      builder: (context, snapshot) {
+        final child = snapshot.connectionState == ConnectionState.waiting
+            ? loadingScreen
+            : appScreen;
+        return AnimatedSwitcher(
+          duration: const Duration(seconds: 1),
+          child: child,
+        );
       },
     );
   }
+
+  Widget get loadingScreen => const MaterialApp(
+        title: 'Flutter Demo',
+        home: LoadingScreen(),
+      );
+
+  Widget get appScreen => BlocProvider(
+      bloc: ExpensesBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        home: const ExpensesPage(),
+        routes: {
+          '/add_expense': (_) => const AddExpense(),
+        },
+      ));
+
+  Future<List> get preloadActions => Future.wait([
+        /// Add here all initialization actions here before the main interface is displayed
+        Future.delayed(const Duration(milliseconds: 1000), () {}),
+        AppDatabase.getInstanceFuture(),
+      ]);
 }
